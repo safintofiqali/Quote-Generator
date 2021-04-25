1d31ea78634c2180bb056f9a17bb4950c6c53234
safintofiqali <safintofiqali@gmail.com>
2021-04-25 21:50:53 +0300

    api setup

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("Testing");
+const quoteContainer = document.querySelector("#quote-container");
+const quoteEl = document.querySelector("#quote");
+const authorText = document.querySelector("#author");
+const twitterBtn = document.querySelector("#twitter");
+const newQuoteBtn = document.querySelector("#new-quote");
+
+let apiQuotes = [];
+// Get Quotes From API
+async function getQuotes() {
+    const apiUrl = "https://type.fit/api/quotes";
+    try {
+        const response = await fetch (apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Show Quote
+function newQuote() {
+    let random = Math.floor(Math.random() * apiQuotes.length);
+    const {author,text} = apiQuotes[random];
+    authorText.textContent = author;
+    quoteEl.textContent = text;
+}
+
+// On Load
+getQuotes();
+
