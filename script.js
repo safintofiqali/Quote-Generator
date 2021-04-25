75d078786cf1b0795e978bb8f093cfaba2175fd4
safintofiqali <safintofiqali@gmail.com>
2021-04-25 22:27:58 +0300

    loader addedk

--- script.js
+++ script.js
@@ -3,10 +3,25 @@ const quoteEl = document.querySelector("#quote");
 const authorText = document.querySelector("#author");
 const twitterBtn = document.querySelector("#twitter");
 const newQuoteBtn = document.querySelector("#new-quote");
+const loader = document.querySelector("#loader");
 
 let apiQuotes = [];
+
+// Loading Function 
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// hide Loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // Get Quotes From API
 async function getQuotes() {
+    loading();
     const apiUrl = "https://type.fit/api/quotes";
     try {
         const response = await fetch (apiUrl);
@@ -19,11 +34,13 @@ async function getQuotes() {
 
 // Show Quote
 function newQuote() {
+    loading();
     let random = Math.floor(Math.random() * apiQuotes.length);
     const {author,text} = apiQuotes[random];
     authorText.textContent = author ?? "Unknown";
     text.length > 120 ? quoteEl.classList.add("long-quote") : quoteEl.classList.remove("long-quote");
     quoteEl.textContent = text;
+    complete();
 }
 
 // Tweet Quote
